1a4f952debb2f84f9dfea39e3914da1f7a584cd2
Esmaeil Ahmadipour <software8686@gmail.com>
2020-04-02 04:48:58 +0430

    Initial commit

---
+++ Assets/Scripts/LoginCheck.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginCheck : MonoBehaviour {
+
+	public  InputField field_Username_login;
+	public  InputField field_Password_login;
+
+	public string inputUserName_login;
+	public string inputPassword_login;
+	public string backData_str_login;
+
+	string LoginUserUrl = "https://ea2.ir/Game_Dir/PHP/GDB_Login.php";
+
+	// Use this for initialization
+	//void Start () {
+	//}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+	}
+
+	// Create Username nad Password
+	public void LoginUser(string username_login , string password_login)
+	{
+		WWWForm form_login = new WWWForm ();
+		form_login.AddField ("US_NAME",username_login);
+		form_login.AddField ("US_PASS",password_login);
+		WWW www = new WWW (LoginUserUrl , form_login);
+		StartCoroutine(WaitForRequest(www));
+
+	}
+
+	public void login_User()
+	{
+		inputUserName_login= field_Username_login.text;
+		inputPassword_login= field_Password_login.text;
+
+		LoginUser (inputUserName_login, inputPassword_login);
+	}
+
+
+	IEnumerator WaitForRequest(WWW www_login)
+	{
+		yield return www_login;
+
+		// check for errors
+		if (www_login.error == null)
+		{
+			Debug.Log("WWW Ok!: " + www_login.text);// contains all the data sent from the server
+		} 
+		else 
+		{
+			Debug.Log("WWW Error: "+ www_login.error);
+		}    
+
+
+
+
+		backData_str_login =www_login.text;
+
+
+
+
+
+		switch (backData_str_login) 
+		{
+		case "Login Successfully":
+			{
+				print ("Login Successfully");
+				SceneManager.LoadScene ("Login_Done");
+			}
+			break;
+
+		case "Login_Failed":
+			{
+				print ("Register_Failed");
+				SceneManager.LoadScene ("Login_Failed");
+
+			}
+			break;
+		}
+	}
+}
+
---
+++ Assets/Scripts/NewUsername.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewUsername : MonoBehaviour {
+
+	public  InputField field_Username;
+	public  InputField field_Password;
+
+	 public string inputUserName;
+	public string inputPassword;
+	public string backData_str;
+
+	string CreateUserUrl = "https://ea2.ir/Game_Dir/PHP/GDB_Register.php";
+
+	// Use this for initialization
+	//void Start () {
+	//}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			CreateUser (inputUserName, inputPassword);
+		}
+			
+	}
+
+	// Create Username nad Password
+	public void CreateUser(string username , string password)
+	{
+		WWWForm form = new WWWForm ();
+		form.AddField ("US_NAME",username);
+		form.AddField ("US_PASS",password);
+		WWW www = new WWW (CreateUserUrl , form);
+		StartCoroutine(WaitForRequest(www));
+
+	}
+
+	public void creat_User()
+	{
+		inputUserName= field_Username.text;
+		inputPassword= field_Password.text;
+
+		CreateUser (inputUserName, inputPassword);
+	}
+
+
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+			// check for errors
+			if (www.error == null)
+				{
+					Debug.Log("WWW Ok!: " + www.text);// contains all the data sent from the server
+				} 
+				else 
+					{
+						Debug.Log("WWW Error: "+ www.error);
+					}    
+
+
+
+
+		backData_str =www.text;
+
+
+
+
+
+		switch (backData_str) 
+		{
+		case "Register_Done":
+			{
+				print ("Register_Done");
+				SceneManager.LoadScene ("SignUp_Done");
+			}
+			break;
+
+		case "Register_Failed":
+			{
+				print ("Register_Failed");
+				SceneManager.LoadScene ("SignUp_Failed");
+
+			}
+			break;
+		case "Duplicate_Username":
+			{
+				print ("Duplicate_Username");
+				SceneManager.LoadScene ("SignUp_Failed2");
+
+			}
+			break;
+		}
+	}
+}
+
